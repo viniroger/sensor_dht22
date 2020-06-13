a3df0c96e94cc7c8d4fe2fc34bb2e63e774c87e1
Vinicius Roggério da Rocha <viniroger@yahoo.com.br>
2020-06-13 11:28:42 -0300

    Print min and max value to terminal

--- plots.py
+++ plots.py
@@ -9,6 +9,8 @@ import matplotlib.dates as mdates
 # Get dataframe from CSV file
 filename = 'data.csv'
 df = pd.read_csv(filename)
+print('Temperature\n max: {0}°C min: {1}°C'.format(min(df.temperature), max(df.temperature)))
+print('Humidity\n max: {0}% min: {1}%'.format(min(df.humidity), max(df.humidity)))
 
 # Convert into datetime format
 x_orig = df.timestamp
